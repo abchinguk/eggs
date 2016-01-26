ea657b255fff239f9b9088a74f87844b6f3453e1
abchinguk <allenetie590@gmail.com>
2016-01-26 12:37:29 +0300

    initial commit

---
+++ eggs.rb
@@ -0,0 +1,17 @@
+# массив яиц, 0 - значит целое, любое другое значение - битое
+eggs = [0, 0, 0, 0, 1, 1, 0, 0, 1, 23, 0, 2, 0, 25, 0, 0, 1, 0]
+
+good_eggs = []
+broken_count = 0
+
+for item in eggs do
+	if(item != 0)  
+		broken_count += 1
+	else
+		good_eggs << item
+	end	
+end	
+
+puts eggs.to_s
+puts good_eggs.to_s
+puts broken_count.to_s	
